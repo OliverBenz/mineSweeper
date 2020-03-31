235ad4dd67caac391055826c35601a955bf96404
unknown <oliver.benz@outlook.com>
2020-03-31 17:56:48 +0200

    REMOVED: practice structure

--- cMain.cpp
+++ cMain.cpp
@@ -1,14 +1,10 @@
 #include "cMain.h"
 
 wxBEGIN_EVENT_TABLE(cMain, wxFrame)
-//EVT_BUTTON(10001, OnButtonClicked)
+
 wxEND_EVENT_TABLE()
 
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Main Window", wxPoint(50, 50), wxSize(800, 600)) {
-	//m_btn1 = new wxButton(this, 10001, "Click Here", wxPoint(10, 10), wxSize(150, 50));
-	//m_txt1 = new wxTextCtrl(this, wxID_ANY, "Placeholder", wxPoint(10, 70), wxSize(300, 30));
-	//m_list1 = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(300, 300));
-
 	// Create buttons and grid
 	btn = new wxButton * [nFieldWidth * nFieldHeight];
 	wxGridSizer* grid = new wxGridSizer(nFieldWidth, nFieldHeight, 0, 0);
@@ -42,8 +38,6 @@ cMain::~cMain() {
 }
 
 void cMain::OnButtonClicked(wxCommandEvent& evt) {
-	//m_list1->AppendString(m_txt1->GetValue());
-
 	int x = (evt.GetId() - 10000) % nFieldWidth;
 	int y = (evt.GetId() - 10000) / nFieldWidth;
 
